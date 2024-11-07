6b794b2f1105a08546200f6b1e8a8d0b2be1ec02
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2024-11-07 18:53:46 +0700

    Update result page

--- Frontend/src/app/(single layout)/result/page.tsx
+++ Frontend/src/app/(single layout)/result/page.tsx
@@ -10,7 +10,6 @@ import { useSearchParams } from "next/navigation";
 import { Buffer } from "buffer";
 import Modification from "./modification";
 import Map_Res from "./map";
-import { clearInterval } from "timers";
 
 interface Object {
   task_id: any;
@@ -40,23 +39,75 @@ interface ExifData {
   };
 }
 
+interface SearchResult {
+  title: string | undefined;
+  redirect_link: string | undefined;
+}
+
 interface Result {
   exif_data: any;
   jpeg_ghost_result: string;
+  reverse_image_search_results: any;
+  super_resolution_result: string;
 }
 const inter = Inter({ subsets: ["latin"] });
 const Result = () => {
   const searchParams = useSearchParams();
   const data = searchParams.get("data");
   const img = searchParams.get("image");
+  const wsRef = useRef<WebSocket | null>(null);
 
   const [taskData, setTaskData] = useState<Object | null>(null);
   const [exifData, setExifData] = useState<ExifData | null>(null);
+  const [searchData, setSearchData] = useState<SearchResult[] | null>(null);
   const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
   const [isFetching, setIsFetching] = useState<Boolean>(false);
 
   const [methodData, setmethodData] = useState<Result | null>(null);
   const [retrieve_img, setRetrieveImg] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchTaskStatus = async () => {
+      try {
+        wsRef.current = new WebSocket('ws://127.0.0.1:8000');
+
+        wsRef.current.onopen = () => {
+          console.log('WebSocket connection opened');
+        };
+
+        wsRef.current.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          setTaskData(data);
+          if (data.status === 'SUCCESS') {
+            setIsFetching(false);
+            console.log('Fetching false');
+            wsRef.current?.close();
+          }
+        };
+
+        wsRef.current.onerror = (error) => {
+          console.error('WebSocket error:', error);
+        };
+
+        wsRef.current.onclose = () => {
+          console.log('WebSocket connection closed');
+        };
+      } catch (error) {
+        console.error('Failed to fetch task status:', error);
+      }
+    };
+
+    fetchTaskStatus();
+
+    return () => {
+      if (wsRef.current) {
+        setIsFetching(false);
+        console.log('Fetching false');
+        wsRef.current.close();
+        wsRef.current = null;
+      }
+    };
+  }, []);
 
   useEffect(() => {
     if (data) {
@@ -134,12 +185,18 @@ const Result = () => {
     if (methodData) {
       console.log(methodData.exif_data);
       setExifData(methodData.exif_data);
+      setSearchData(methodData.reverse_image_search_results.image_results);
     }
   }, [methodData]);
   return (
     <>
       <div className={`Top-container ${inter.className}`}>
-        <div className={`Confident-score-container ${inter.className} font-bold`}> The confident score: </div>
+        <div
+          className={`Confident-score-container ${inter.className} font-bold`}
+        >
+          {" "}
+          The confident score:{" "}
+        </div>
         <div className="vertical-space"></div>
         <div className="General-report-container">
           <Image src={pattern} width={200} height={200} alt={""}></Image>
@@ -173,23 +230,37 @@ const Result = () => {
             <Map_Res gps_location={exifData?.gps_location} />
           </div>
           <div className="Result-container">
-              <div className='w-full h-full p-5'>
-          <div className='flex'>
-            <div className='circle_2'> 5. </div>
-          <div className='font-bold text-lg ml-2 mb-5'>JPEG Ghost</div>
+            <div className="w-full h-full p-5">
+              <div className="flex">
+                <div className="circle_2"> 5. </div>
+                <div className="font-bold text-lg ml-2 mb-5">JPEG Ghost</div>
+              </div>
+              {methodData?.jpeg_ghost_result && (
+                <Image
+                  src={`data:image/jpeg;base64,${methodData.jpeg_ghost_result}`}
+                  alt=""
+                  width={500}
+                  height={500}
+                />
+              )}
+            </div>
           </div>
-                  {methodData?.jpeg_ghost_result && (
-                    <Image src={`data:image/jpeg;base64,${methodData.jpeg_ghost_result}`} alt="" width={500} height={500}/>
-                  )}
+          <div className="Result-container">
+            {" "}
+            <div className="w-full h-full p-5">
+              <div className="flex">
+                <div className="circle_2"> 6. </div>
+                <div className="font-bold text-lg ml-2 mb-5">
+                  Reversed Image Search
+                </div>
+              </div>
+              {searchData?.map((result, index) => (
+                <div key={index} >
+                  <a href={result.redirect_link} className="hover: underline">{index+1}. {result.title}</a>
+                </div>
+              ))}
+            </div>
           </div>
-          </div>
-          <div className="Result-container">  <div className='w-full h-full p-5'>
-    <div className='flex'>
-       <div className='circle_2'> 6. </div>
-    <div className='font-bold text-lg ml-2 mb-5'>Reversed Image Search</div>
-    </div>
-    </div>
-    </div>
         </div>
       </div>
       <div className="flex flex-col items-center"></div>
