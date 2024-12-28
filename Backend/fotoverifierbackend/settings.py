34b414475f756235e1cf2d4359abcac33b2d5a2d
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2024-12-28 20:42:25 +0700

    Fix docker

--- Backend/fotoverifierbackend/settings.py
+++ Backend/fotoverifierbackend/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'fotoverifierbackend.urls'
@@ -131,11 +131,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:9016",
-    "http://fotoverifier.eu:9016",
 ]
 
 
