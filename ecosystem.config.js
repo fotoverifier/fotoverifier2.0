16d19645777d40d0dd4b54380e7810bc8dcc5133
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2025-03-19 14:37:14 +0700

    fix: update requirements.txt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     name   : "fastapi",
     script : "uvicorn",
     args: "main:app --host 0.0.0.0 --port 9001",
-    interpreter: "/home/ubuntu/fotoverifier2.0/Backend/venv/bin/python",
+    interpreter: "/home/ubuntu/fotoverifier2.0/Backend_new/venv/bin/python",
     cwd: "./Backend_new",
     watch: false,
     env: {
@@ -19,8 +19,8 @@ module.exports = {
   {
     name   : "celery",
     script : "celery",
-    args: "-A fotoverifierbackend worker --loglevel=info -c 4",
-    interpreter: "/home/ubuntu/fotoverifier2.0/Backend/venv/bin/python",
+    args: "-A tasks worker --loglevel=info -c 4",
+    interpreter: "/home/ubuntu/fotoverifier2.0/Backend_new/venv/bin/python",
     cwd: "./Backend_new",
     watch: false,
     env: {
