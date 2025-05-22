59639a366aeb507347d09a4ae3ca36ea5dafb066
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2025-05-22 13:42:10 +0700

    set 2 celery workers

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
   {
     name   : "celery",
     script: "celery",
-    args: "-A tasks worker --loglevel=info -c 1",
+    args: "-A tasks worker --loglevel=info -c 2",
     interpreter: "python3",
     cwd: "./Backend_new",
     watch: false,
