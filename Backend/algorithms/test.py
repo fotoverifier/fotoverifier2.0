a5d6206c1368ef011b50c9f09015f167d919523f
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2025-07-22 21:27:45 +0700

    remove old backend folder

--- Backend/algorithms/test.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-try:
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.ping()  # This will return True if Redis is running
-    print("Redis is running.")
-except redis.ConnectionError:
-    print("Redis is not running.")
--- Backend/image/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from .consumer import TaskConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/tasks/(?P<task_id>\d+)/$', TaskConsumer.as_asgi()),
-]
